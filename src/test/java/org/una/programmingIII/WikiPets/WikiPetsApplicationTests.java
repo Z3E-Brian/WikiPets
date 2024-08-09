d44970379e94ce4d2d1986e6405f4c592243c427
Breiner Muñoz Fallas <breiner.munoz.fallas@est.una.ac.cr>
2024-08-09 14:01:27 -0600

    [ADD] Spring boot settings and pom

---
+++ src/test/java/org/una/programmingIII/WikiPets/WikiPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.programmingIII.WikiPets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
