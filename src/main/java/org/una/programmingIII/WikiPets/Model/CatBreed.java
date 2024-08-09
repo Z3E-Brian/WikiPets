0549a52e2a49798317ba87543a6f7e5f7a3cf49a
Breiner Muñoz Fallas <breiner.munoz.fallas@est.una.ac.cr>
2024-08-09 16:50:00 -0600

    [ADD]Entity CatBreed and CatBreedDto

---
+++ src/main/java/org/una/programmingIII/WikiPets/Model/CatBreed.java
@@ -0,0 +1,31 @@
+package org.una.programmingIII.WikiPets.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "cat_breeds")
+public class CatBreed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100,nullable = false)
+    private String name;
+    @Column(length = 100,nullable = false)
+    private String origin;
+    @Column(length = 4,nullable = false)
+    private Integer size;
+    @Column(length = 50,nullable = false)
+    private String coat;
+    @Column(length = 50,nullable = false)
+    private String color;
+    @Column(length = 50,nullable = false, name = "life_span")
+    private String lifeSpan;
+    @Column(length = 50,nullable = false)
+    private String temperament;
+    @Column(length = 2000,nullable = false)
+    private String description;
+}
