8f4beefb932a3f91ea79f94b23519553a19dd3e6
Brian Zeledón <brian.zeledon.esquivel@est.una.ac.cr>
2024-08-16 19:46:28 -0600

    [ADD] Added the remaining entities from me
    [TODO] Services and mappers

---
+++ src/main/java/org/una/programmingIII/WikiPets/Model/VideoDto.java
@@ -0,0 +1,20 @@
+package org.una.programmingIII.WikiPets.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VideoDto {
+    private Long id;
+    private String url;
+    private String description;
+    private Boolean isDogBreed;
+    private int petBreedId;
+    private LocalDate createDate;
+    private LocalDate lastUpdate;
+}
