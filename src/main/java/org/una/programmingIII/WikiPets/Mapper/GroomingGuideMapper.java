6923da096bfba9f2effa8eabcc0c7dc666c17aef
Brian Zeledón <brian.zeledon.esquivel@est.una.ac.cr>
2024-08-18 16:54:26 -0600

    [ADD] Added mappers for clases 6 to 10
    Classes added for me

---
+++ src/main/java/org/una/programmingIII/WikiPets/Mapper/GroomingGuideMapper.java
@@ -0,0 +1,16 @@
+package org.una.programmingIII.WikiPets.Mapper;
+
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import org.una.programmingIII.WikiPets.Model.GroomingGuide;
+import org.una.programmingIII.WikiPets.Model.GroomingGuideDto;
+
+public interface GroomingGuideMapper {
+    GroomingGuideMapper INSTANCE = Mappers.getMapper(GroomingGuideMapper.class);
+    @Mapping(target = "availableDogBreeds", source = "availableDogBreeds")
+    @Mapping(target = "availableCatBreeds", source = "availableCatBreeds")
+    GroomingGuideDto toGroomingGuideDto(GroomingGuide groomingGuide);
+    @Mapping(target = "availableDogBreeds", source = "availableDogBreeds")
+    @Mapping(target = "availableCatBreeds", source = "availableCatBreeds")
+    GroomingGuide toGroomingGuide(GroomingGuideDto groomingGuideDto);
+}
